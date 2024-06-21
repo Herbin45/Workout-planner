ebf4019602645ef425ea3d1ddef0d6e9d4c7a0e6
Christofer D <kristofferdanielsson@hotmail.com>
2024-06-21 08:11:14 +0200

    Initial configuration step

---
+++ src/main/java/edu/christofer/danielsson/workout/WorkoutPlannerApplication.java
@@ -0,0 +1,13 @@
+package edu.christofer.danielsson.workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPlannerApplication.class, args);
+	}
+
+}
